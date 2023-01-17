21b2cbe0ea534992d4b12d8a0609c7022d0410d2
johary1 <joharisoa@gmail.com>
2023-01-17 15:02:07 +0100

    add attributes to video and alternative text

--- scripts/factories/videoFactory.js
+++ scripts/factories/videoFactory.js
@@ -7,7 +7,9 @@ export default class VideoFactory {
     eltVideo.setAttribute("controls", "controls");
     eltVideo.setAttribute("src", el.video);
     eltVideo.setAttribute("role", "button");
+    eltVideo.setAttribute("type", "video/mp4");
     eltVideo.className = "ph-media";
+    eltVideo.textContent = "Your browser does not support HTML5 videos.";
 
     return eltVideo;
   }
